a29f363cc9255eb9a80f7b81aba926f1bfc6a00a
Saad Adel <saad.adel539@yahoo.com>
2020-10-23 18:06:56 +0200

    add currency converter

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,12 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Swap\Laravel\Facades\Swap;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'price'];
+
+    public function getPriceAttribute($price)
+    {
+        $currency = app('request')->header('currency', 'USD');
+ 
+        if($currency->code != 'USD')
+        {
+            $rate = Swap::latest('USD/' . $currency->code);
+
+            $price *= $rate->getValue();
+        }
+
+        $price = round((float) $price, 2);
+
+        return $price;
+    }
 }
