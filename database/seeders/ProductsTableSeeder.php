d8a57e3717c2729f808b24809072fc62570c56c8
Saad Adel <saad.adel539@yahoo.com>
2020-10-23 17:41:51 +0200

    make products

---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $products = [
+            [
+                'name' => 'T-shirt',
+                'price' => 10.99
+            ],
+            [
+                'name' => 'Pants',
+                'price' => 14.99
+            ],
+            [
+                'name' => 'Jaket',
+                'price' => 19.99
+            ],
+            [
+                'name' => 'Shoes',
+                'price' => 24.99
+            ]
+        ];
+
+        foreach ($products as $product) {
+            Product::create([
+                'name' => $product['name'],
+                'price' => $product['price']
+            ]);
+        }
+    }
+}
